e38595cbfc2bcb41c787f68df299956a8be6362d
jmaneztech <proyectos@jmanez.es>
2025-09-17 03:23:20 +0200

    Primer commit - proyecto inversiones

---
+++ src/main/java/com/jmanez/calculadora/inversiones/CalculadoraInversionesApplication.java
@@ -0,0 +1,13 @@
+package com.jmanez.calculadora.inversiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraInversionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraInversionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmanez/calculadora/inversiones/CalculadoraInversionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmanez.calculadora.inversiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraInversionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
